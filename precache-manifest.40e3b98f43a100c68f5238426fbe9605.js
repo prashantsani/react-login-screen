3a22e0ef53ac9134608a8d9436245b26b39d2aff
Prashant Sani <contact@prashantsani.com>
2020-05-21 03:42:27 +0530

    Updates

---
+++ precache-manifest.40e3b98f43a100c68f5238426fbe9605.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a15d24ae0d6f7e51e8d7b4238715503",
+    "url": "/react-login-screen/index.html"
+  },
+  {
+    "revision": "f21e6b8118918239e592",
+    "url": "/react-login-screen/static/css/main.1f52995b.chunk.css"
+  },
+  {
+    "revision": "74a4846875b21357ef9a",
+    "url": "/react-login-screen/static/js/2.a94feea1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-login-screen/static/js/2.a94feea1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f21e6b8118918239e592",
+    "url": "/react-login-screen/static/js/main.d1ab2e99.chunk.js"
+  },
+  {
+    "revision": "fca505cdc00d17ba9d64",
+    "url": "/react-login-screen/static/js/runtime-main.e668124d.js"
+  }
+]);
